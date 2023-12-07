c74d8c6066974f12895b3abdb7750ec3c330dab2
festinuz <festinuz@gmail.com>
2023-12-07 12:44:36 +0300

    Store secret in file & support multiple proxy ports

--- cli/main.go
+++ cli/main.go
@@ -2,83 +2,47 @@ package main
 
 import (
 	"bytes"
-	"flag"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 	wifiname "github.com/yelinaung/wifi-name"
 )
 
 const (
-	uploadURL        = "http://127.0.0.1:8008/upload"
-	wsURL            = "ws://127.0.0.1:8008/ws"
-	pacURLFormat     = "http://127.0.0.1:8008/pac/%s"
+	uploadURL    = "http://127.0.0.1:8008/upload"
+	wsURL        = "ws://127.0.0.1:8008/ws"
+	pacURLFormat = "http://127.0.0.1:8008/pac/%s"
+)
+
+const (
+	configFilename   = ".mitmpac.config"
 	defaultProxyPort = "8080"
+	pacProxyFormat   = `PROXY %s:%s`
 	pacFormat        = `function FindProxyForURL(url, host) {
-    return "PROXY %s:%s";
+    return "%s";
 }`
 )
 
-func main() {
-	secret := flag.String("secret", "", "unique secret value to identify the user")
-	port := flag.String("port", "", "target port of proxy")
-	flag.Parse()
+var defaultProxyPorts = []string{"8888", "8080"}
 
-	if *secret == "" {
-		fmt.Println("Please provide a unique secret value with --secret")
-		return
-	}
+type FileCOnfig struct {
+	Secret string `json:"secret"`
+}
 
-	proxyPort := defaultProxyPort
-	if *port != "" {
-		proxyPort = *port
-	}
-
-	ip := getLocalIP()
-	networkName := wifiname.WifiName()
-	if ip == "" {
-		fmt.Println("Failed to get local IP.")
-		return
-	}
-
-	fmt.Printf("Network name: %s\n", networkName)
-	fmt.Printf("Local IP: %s\n", ip)
-
-	pacContent := fmt.Sprintf(pacFormat, ip, proxyPort)
-
-	fmt.Printf("Generated PAC configuration: \n\n%s\n\n", pacContent)
-
-	configID, err := uploadPAC(pacContent, *secret)
-	if err != nil {
-		fmt.Println("Failed to upload PAC:", err)
-		return
-	}
-
-	pacURL := fmt.Sprintf(pacURLFormat, configID)
-	fmt.Println("PAC URL:", pacURL)
-
-	header := http.Header{}
-	header.Set("X-Secret", *secret)
-	client, _, err := websocket.DefaultDialer.Dial(wsURL, header)
-	if err != nil {
-		fmt.Println("Failed to connect to websocket server:", err)
-		return
-	}
-	defer client.Close()
-
-	fmt.Println("PAC uploaded successfully. Press Ctrl+C to stop serving.")
-	for {
-		_, message, err := client.ReadMessage()
-		if err != nil {
-			fmt.Println("Websocket error, closing connection:", err)
-			return
-		}
-		fmt.Println("Message from server:", string(message))
-	}
+type Config struct {
+	Secret     string
+	LocalIp    string
+	ProxyPorts []string
 }
 
 func getLocalIP() string {
@@ -94,6 +58,86 @@ func getLocalIP() string {
 		}
 	}
 	return ""
+}
+
+func getUserSecret() (secret string, err error) {
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	configpath := filepath.Join(homedir, configFilename)
+
+	if _, err := os.Stat(configpath); errors.Is(err, os.ErrNotExist) {
+		fmt.Println("No config file found: ", configpath)
+		newSecret := uuid.New().String()
+		fileData := FileCOnfig{
+			Secret: newSecret,
+		}
+		file, err := json.Marshal(fileData)
+		if err != nil {
+			return "", err
+		}
+		fmt.Println("Creating new config file in user directory")
+		err = os.WriteFile(configpath, file, 0644)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	fmt.Println("Reading secret from config file")
+	configFile, err := os.Open(configpath)
+	if err != nil {
+		return "", err
+	}
+	defer configFile.Close()
+	configValue, err := io.ReadAll(configFile)
+	if err != nil {
+		return "", err
+	}
+
+	var fileCOnfig FileCOnfig
+	err = json.Unmarshal(configValue, &fileCOnfig)
+	if err != nil {
+		return "", err
+	}
+
+	return fileCOnfig.Secret, nil
+}
+
+func getConfig() (config Config, err error) {
+	secret, err := getUserSecret()
+	if err != nil {
+		return config, err
+	}
+	config.Secret = secret
+
+	ip := getLocalIP()
+	if ip == "" {
+		return config, errors.New("Failed to get local IP.")
+	}
+	config.LocalIp = ip
+
+	config.ProxyPorts = defaultProxyPorts
+
+	return config, nil
+}
+
+func printNetworkName() {
+	networkName := wifiname.WifiName()
+
+	fmt.Printf("Network name: %s\n", networkName)
+}
+
+func createPAC(config Config) (PAC string) {
+	var proxies []string
+	for _, proxyPort := range config.ProxyPorts {
+		proxy := fmt.Sprintf(pacProxyFormat, config.LocalIp, proxyPort)
+		proxies = append(proxies, proxy)
+	}
+	proxySetting := strings.Join(proxies, "; ")
+	PAC = fmt.Sprintf(pacFormat, proxySetting)
+	return PAC
 }
 
 func uploadPAC(pacContent string, secret string) (string, error) {
@@ -121,4 +165,49 @@ func uploadPAC(pacContent string, secret string) (string, error) {
 		return "", err
 	}
 	return string(body), nil
+}
+
+func listenForWebsocketUpdates(secret string) {
+	headers := http.Header{}
+	headers.Set("X-Secret", secret)
+	client, _, err := websocket.DefaultDialer.Dial(wsURL, headers)
+	if err != nil {
+		fmt.Println("Failed to connect to websocket server:", err)
+		return
+	}
+	defer client.Close()
+
+	fmt.Println("PAC uploaded successfully. Press Ctrl+C to stop serving.")
+	for {
+		_, message, err := client.ReadMessage()
+		if err != nil {
+			fmt.Println("Websocket error, closing connection:", err)
+			return
+		}
+		fmt.Println("Message from server:", string(message))
+	}
+}
+
+func main() {
+	config, err := getConfig()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	printNetworkName()
+
+	pacContent := createPAC(config)
+	fmt.Printf("Generated PAC configuration: \n\n%s\n\n", pacContent)
+
+	configID, err := uploadPAC(pacContent, config.Secret)
+	if err != nil {
+		fmt.Println("Failed to upload PAC:", err)
+		return
+	}
+
+	pacURL := fmt.Sprintf(pacURLFormat, configID)
+	fmt.Println("PAC URL:", pacURL)
+
+	listenForWebsocketUpdates(config.Secret)
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module mitmpac
 go 1.21
 
 require (
+	github.com/google/uuid v1.4.0
 	github.com/gorilla/websocket v1.5.1
 	github.com/yelinaung/wifi-name v0.0.0-20181205043121-60d8acb81b8f
 )
 
-require golang.org/x/net v0.17.0 // indirect
+require golang.org/x/net v0.19.0 // indirect
